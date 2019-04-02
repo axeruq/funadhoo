41c0355cf14540f2f546f9a7872af5bffceba56c
Usman Mohamed <47541711+uMnNoor@users.noreply.github.com>
2019-04-02 19:29:20 +0500

    Update map.js

--- map.js
+++ map.js
@@ -3,10 +3,14 @@ var input = document.querySelector("#geygeNan");
 var sButton = document.querySelector("#hodhaa");
 var hideMarker = document.querySelector("#hideMarker");
 var uLocation = document.querySelector("#uLocation");
+var errorMsg = document.querySelector("#content");
+var okayBtn = document.querySelector("#okay");
+var toggleBtn = document.querySelector("#toggle");
+var infoContent = document.querySelector("#infoTag");
 
 var funadhoo = {lat: 6.1487733, lng: 73.2882523};
 
-var cGe, cPos, map, geMark, popup, Popup;
+var cGe, cPos, map, geMark, popup, Popup, title;
 
 var geArray = [];
 var posArray = [];
@@ -37,75 +41,80 @@ function showPosition(position) {
     cPos = {lat: clat, lng: clng};
 }
 
-sButton.addEventListener("click", function() {
-
-  for (let i = 0; i < geygeList.length; i++) {
-    if (input.value.toUpperCase() === geygeList[i].name.toUpperCase()) {
-      cGe = {lat: geygeList[i].lat, lng: geygeList[i].lng}
-      break;
-    }
-  }
-
+function getAddress() {
   if (geArray.length > 0) {
     geArray[0].setMap(null);
     geArray.shift();
   }
 
-  var marker = new google.maps.Marker({
-    position: cGe,
-    map: map,
-    animation: google.maps.Animation.DROP});
+  for (let i = 0; i < geygeList.length; i++) {
+    if (input.value.toUpperCase() === geygeList[i].name.toUpperCase()) {
+      cGe = {lat: geygeList[i].lat, lng: geygeList[i].lng}
+      title = geygeList[i].name;
+      var marker = new google.maps.Marker({
+        position: cGe,
+        map: map,
+        title: title,
+        animation: google.maps.Animation.DROP});
 
-    geArray.push(marker);
+        let gRoad = geygeList[i].road;
+        let gNo = geygeList[i].houseNo;
+        let gBlock = geygeList[i].blockNo;
 
-    geMarker = marker;
-    map.setZoom(18);
-    map.setCenter(geMarker.getPosition());
+        let infoBarN = document.createElement("p");
+        let infoBarR = document.createElement("p");
+        let infoBarNo = document.createElement("p");
+        let infoBarBNo = document.createElement("p");
+
+        let textBarN = document.createTextNode(`House Name: ${title}`);
+        let textBarR = document.createTextNode(`Road: ${gRoad}`);
+        let textBarNo = document.createTextNode(`House Number: ${gNo}`);
+        let textBarBNo = document.createTextNode(`Block Number: ${gBlock}`);
+
+        infoBarN.appendChild(textBarN);
+        infoBarR.appendChild(textBarR);
+        infoBarNo.appendChild(textBarNo);
+        infoBarBNo.appendChild(textBarBNo);
+
+        infoContent.appendChild(infoBarN);
+        infoContent.appendChild(infoBarR);
+        infoContent.appendChild(infoBarNo);
+        infoContent.appendChild(infoBarBNo);
+
+        geArray.push(marker);
+
+        geMarker = marker;
+        map.setZoom(18);
+        map.setCenter(geMarker.getPosition());
 
 
-    geMarker.addListener('click', function() {
-    map.setZoom(18);
-    map.setCenter(geMarker.getPosition());
-  });
+        geMarker.addListener('click', function() {
+        map.setZoom(18);
+        map.setCenter(geMarker.getPosition());
+      });
 
 
+      input.value = "";
+      var matchFound = true;
+      break;
+    } else {
+      var matchFound = false;
+    }
+  }
+  if (matchFound == false) {
+    errorMsg.classList.remove("hide");
+    errorMsg.classList.add("error");
+  }
   input.value = "";
+}
+
+sButton.addEventListener("click", function() {
+  getAddress();
 });
 
 form.addEventListener("submit", function(e) {
   e.preventDefault();
-
-  for (let i = 0; i < geygeList.length; i++) {
-    if (input.value.toUpperCase() === geygeList[i].name.toUpperCase()) {
-      cGe = {lat: geygeList[i].lat, lng: geygeList[i].lng}
-      break;
-    }
-  }
-
-  if (geArray.length > 0) {
-    geArray[0].setMap(null);
-    geArray.shift();
-  }
-
-  var marker = new google.maps.Marker({
-    position: cGe,
-    map: map,
-    animation: google.maps.Animation.DROP});
-
-    geArray.push(marker);
-
-    geMarker = marker;
-    map.setZoom(18);
-    map.setCenter(geMarker.getPosition());
-
-
-    geMarker.addListener('click', function() {
-    map.setZoom(18);
-    map.setCenter(geMarker.getPosition());
-  });
-
-
-  input.value = "";
+  getAddress();
 });
 
 uLocation.addEventListener("click", function() {
@@ -120,7 +129,7 @@ uLocation.addEventListener("click", function() {
     var marker = new google.maps.Marker({position: cPos, map: map});
     posArray.push(marker);
 
-    map.setZoom(16);
+    map.setZoom(14);
 
     if (posArray.length > 1) {
       posArray[0].setMap(null);
@@ -141,5 +150,26 @@ hideMarker.addEventListener("click", function() {
     geArray[0].setMap(map);
     markerHidden = false;
     hideMarker.innerHTML = "Hide Marker";
+  }
+});
+
+okayBtn.addEventListener("click", function() {
+  errorMsg.classList.remove("error");
+  errorMsg.classList.add("hide");
+});
+
+toggleBtn.addEventListener("click", function(){
+  if (toggleBtn.classList == "toggle-up") {
+    infoContent.classList.remove("hide");
+    infoContent.classList.add("info-bar");
+    toggleBtn.classList.remove("toggle-up");
+    toggleBtn.classList.add("toggle-down");
+    toggleBtn.setAttribute("src", "img/tdown.png");
+  } else {
+    infoContent.classList.remove("info-bar");
+    infoContent.classList.add("hide");
+    toggleBtn.classList.remove("toggle-down");
+    toggleBtn.classList.add("toggle-up");
+    toggleBtn.setAttribute("src", "img/tup.png")
   }
 });
