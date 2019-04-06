fc1a4b9e322233b6b220cd4ae8a86b20ece2637e
Usman Mohamed <47541711+uMnNoor@users.noreply.github.com>
2019-04-06 07:23:15 +0500

    Update map.js

--- map.js
+++ map.js
@@ -1,25 +1,11 @@
-var form = document.querySelector("form");
-var input = document.querySelector("#geygeNan");
-var sButton = document.querySelector("#hodhaa");
-var hideMarker = document.querySelector("#hideMarker");
-var uLocation = document.querySelector("#uLocation");
-var errorMsg = document.querySelector("#content");
-var okayBtn = document.querySelector("#okay");
-var toggleBtn = document.querySelector("#toggle");
-var infoContent = document.querySelector("#infoTag");
-var list = document.querySelector("#gethah");
-
 var funadhoo = {lat: 6.1487733, lng: 73.2882523};
-
-var cGe, cPos, map, geMark, popup, Popup, title;
-
-var listArray = [];
+var cGe, cPos, map, geMark, title;
 var geArray = [];
 var posArray = [];
-
-var markerHidden = false;
-var uMarkerHidden = false;
-var geInfoSet = false;
+var matchFound;
+var gRoad, gNo, gBlock;
+var houseIcon = "img/houseMarker.png";
+var userIcon = "img/userMarker.png";
 
 function initMap() {
   getLocation();
@@ -45,15 +31,15 @@ function showPosition(position) {
 }
 
 function getAddress() {
-    
+
     if (listArray.length !==0) {
         for ( let k = 0; k < listArray.length; k++) {
             list.removeChild(list.childNodes[0]);
         }
     }
-    
+
     listArray = [];
-    
+
   if (geArray.length > 0) {
     geArray[0].setMap(null);
     geArray.shift();
@@ -67,42 +53,15 @@ function getAddress() {
         position: cGe,
         map: map,
         title: title,
-        animation: google.maps.Animation.DROP});
+        animation: google.maps.Animation.DROP,
+        icon: houseIcon
+      });
 
-        let gRoad = geygeList[i].road;
-        let gNo = geygeList[i].houseNo;
-        let gBlock = geygeList[i].blockNo;
+        gRoad = geygeList[i].road;
+        gNo = geygeList[i].houseNo;
+        gBlock = geygeList[i].blockNo;
 
-        if (geInfoSet == true) {
-          for(let i = 0; i < 4; i++) {
-            infoContent.removeChild(infoContent.childNodes[0]);
-          }
-        }
-        let infoBarN = document.createElement("p");
-        infoBarN.classList.add("pN");
-        let infoBarR = document.createElement("p");
-        infoBarR.classList.add("pR");
-        let infoBarNo = document.createElement("p");
-        infoBarNo.classList.add("pNo")
-        let infoBarBNo = document.createElement("p");
-        infoBarBNo.classList.add("pBNo");
-
-        let textBarN = document.createTextNode(title);
-        let textBarR = document.createTextNode(gRoad);
-        let textBarNo = document.createTextNode(gNo);
-        let textBarBNo = document.createTextNode(gBlock);
-
-        infoBarN.appendChild(textBarN);
-        infoBarR.appendChild(textBarR);
-        infoBarNo.appendChild(textBarNo);
-        infoBarBNo.appendChild(textBarBNo);
-
-        infoContent.appendChild(infoBarN);
-        infoContent.appendChild(infoBarR);
-        infoContent.appendChild(infoBarNo);
-        infoContent.appendChild(infoBarBNo);
-
-        geInfoSet = true;
+        geInfoSetter();
 
         geArray.push(marker);
 
@@ -118,95 +77,36 @@ function getAddress() {
 
 
       input.value = "";
-      var matchFound = true;
+      matchFound = true;
+      form.classList.add("hidden");
+      searchImg.classList.remove("clickPos");
+      searchImg.classList.add("initPos");
+      userPosImg.classList.remove("img-hidden");
+      housePosImg.classList.remove("img-hidden");
+      housePosImg.setAttribute("src", "img/housePosFound.png");
+      searchClicked = false;
       break;
     } else {
-      var matchFound = false;
+      matchFound = false;
     }
   }
   if (matchFound == false) {
-    errorMsg.classList.remove("hide");
+    errorMsg.classList.remove("hidden");
     errorMsg.classList.add("error");
   }
   input.value = "";
 }
 
-sButton.addEventListener("click", function() {
-  getAddress();
-});
-
-form.addEventListener("submit", function(e) {
-  e.preventDefault();
-  getAddress();
-});
-
-uLocation.addEventListener("click", function() {
-  if (uMarkerHidden === true) {
-      for(let i = 0; i < posArray.length; i++) {
-        posArray[i].setMap(null);
-      }
-    uMarkerHidden = false;
-    uLocation.innerHTML = "Show My Location";
-  } else {
-    getLocation();
-    var marker = new google.maps.Marker({position: cPos, map: map});
-    posArray.push(marker);
-
-    map.setZoom(14);
-
-    if (posArray.length > 1) {
-      posArray[0].setMap(null);
-      posArray.shift();
-    }
-    uMarkerHidden = true;
-    uLocation.innerHTML = "Hide My Location";
-  }
-});
-
-hideMarker.addEventListener("click", function() {
-  if (markerHidden === false) {
-    geArray[0].setMap(null);
-    markerHidden = true;
-    map.setZoom(16);
-    hideMarker.innerHTML = "Show Marker";
-  } else {
-    geArray[0].setMap(map);
-    markerHidden = false;
-    hideMarker.innerHTML = "Hide Marker";
-  }
-});
-
-okayBtn.addEventListener("click", function() {
-  errorMsg.classList.remove("error");
-  errorMsg.classList.add("hide");
-});
-
-toggleBtn.addEventListener("click", function(){
-  if (toggleBtn.classList == "toggle-up") {
-    infoContent.classList.remove("hide");
-    infoContent.classList.add("info-bar");
-    toggleBtn.classList.remove("toggle-up");
-    toggleBtn.classList.add("toggle-down");
-    toggleBtn.setAttribute("src", "img/tdown.png");
-  } else {
-    infoContent.classList.remove("info-bar");
-    infoContent.classList.add("hide");
-    toggleBtn.classList.remove("toggle-down");
-    toggleBtn.classList.add("toggle-up");
-    toggleBtn.setAttribute("src", "img/tup.png")
-  }
-});
-
 input.addEventListener("keydown", function() {
-    
+
     if (listArray.length !==0) {
         for ( let k = 0; k < listArray.length; k++) {
             list.removeChild(list.childNodes[0]);
         }
     }
-    
+
     listArray = [];
-    
+
     if(input.value.length > 1) {
          for(let i = 0; i < geygeList.length; i++) {
             if(geygeList[i].name.toUpperCase().indexOf(input.value.toUpperCase()) > -1) {
@@ -220,5 +120,5 @@ input.addEventListener("keydown", function() {
             optionList.appendChild(optionText);
             list.appendChild(optionList);
     }
-    
+
 });
